b3a88b0196149b51b952f47d407d1bb8ba6cd225
DarkSoul94 <tischenkoslava92@gmail.com>
2021-11-18 10:54:25 +0200

    update docs

--- docs/2.0.0/cat_end_sec/CreateCategorySection.go
+++ docs/2.0.0/cat_end_sec/CreateCategorySection.go
@@ -11,13 +11,14 @@ package cat_end_sec
 *
 * @apiHeader {String} BearerToken Авторизационный токен
 *
-* @apiParam {Uint64} 	section_id 		ID раздела категории. Для создания раздела должен равнятся 0
-* @apiParam {String} 	section_name 	Имя нового раздела категории. Уникально в рамках одной категории
-* @apiParam {Bool} 		significant 	Признак высокого приоритета у раздела
-* @apiParam {Bool} 		old_category 	Признак того что раздел категории устарел
-* @apiParam {Bool} 		need_approval 	Признак того что для раздела необходимо согласование
-* @apiParam {String} 	template 		Шаблон заполнения запроса
-* @apiParam {Uint64} 	category_id 	ID категории
+* @apiParam {Uint64} 	section_id 			ID раздела категории. Для создания раздела должен равнятся 0
+* @apiParam {String} 	section_name 		Имя нового раздела категории. Уникально в рамках одной категории
+* @apiParam {Bool} 		significant 		Признак высокого приоритета у раздела
+* @apiParam {Bool} 		old_category 		Признак того что раздел категории устарел
+* @apiParam {Bool} 		need_approval 		Признак того что для раздела необходимо согласование
+* @apiParam {String} 	template 			Шаблон заполнения запроса
+* @apiParam {Uint64} 	category_id 		ID категории
+* @apiParam {[]Uint64} 	approval_groups 	ID групп которые будут согласовывать запросы с этим разделом категорий
 *
 *
 * @apiParamExample {json} Запрос на создание только раздела:
@@ -29,6 +30,7 @@ package cat_end_sec
 *    "need_approval": false,
 *	 "template":"template for ticket",
 *	 "category_id": 2,
+*	 "approval_groups": [1, 2]
 * }
 *
 * @apiSuccess (Success 200) {Uint64} 	category_section_id 	ID нового раздела категории
