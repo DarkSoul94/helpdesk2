0c1b625922eb8a7758104ee8c9a4b5e3a5916943
IrDeTen <kelxus1@gmail.com>
2021-12-08 12:51:06 +0200

    feat: add penalty to SupportsShiftsMotivation

--- pkg_consts/IRepo.go
+++ pkg_consts/IRepo.go
@@ -1,9 +1,13 @@
 package pkg_consts
 
+import "time"
+
 type IConstsRepo interface {
 	SetConst(key string, data interface{}) error
 	GetConst(key string, target interface{}) error
+
 	CreateHistory(key string, data interface{}) error
+	GetHistory(date time.Time, key string, target interface{}) error
 
 	Close() error
 }
--- pkg_consts/consts list.go
+++ pkg_consts/consts list.go
@@ -1,6 +1,8 @@
 package pkg_consts
 
 const (
-	KeyBanner = "banner"
-	KeyConfig = "config"
+	KeyBanner         = "banner"
+	KeyConfig         = "config"
+	KeyConfig_Penalty = "late_penalty"
+	KeyConfig_Grace   = "grace_time"
 )
