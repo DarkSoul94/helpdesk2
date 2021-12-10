2a156784c7d69519e4c2dcb6c9704cf280c69526
IrDeTen <kelxus1@gmail.com>
2021-12-10 12:35:47 +0200

    fix motivation

--- pkg_reports/usecase/helpers.go
+++ pkg_reports/usecase/helpers.go
@@ -3,7 +3,7 @@ package usecase
 import "github.com/DarkSoul94/helpdesk2/pkg_reports/internal_models"
 
 func (u *ReportsUsecase) summaryMotivation(motivation map[string][]internal_models.Motivation) map[string][]internal_models.Motivation {
-	const total_motiv string = "Суммарная мотивация за период"
+	const total_motiv string = "Общая мотивация за период"
 
 	resultMap := make(map[string][]internal_models.Motivation)
 	resultMap[total_motiv] = make([]internal_models.Motivation, 0)
@@ -11,7 +11,6 @@ func (u *ReportsUsecase) summaryMotivation(motivation map[string][]internal_mode
 	result := make(map[uint64]internal_models.Motivation)
 
 	for period, motiv := range motivation {
-		//TODO расчет суммы мотивации
 		for _, supp := range motiv {
 			resMotiv, ok := result[supp.Support.ID]
 			if !ok {
@@ -28,6 +27,9 @@ func (u *ReportsUsecase) summaryMotivation(motivation map[string][]internal_mode
 			resMotiv.Total = supp.Total.Add(resMotiv.Total)
 		}
 		resultMap[period] = motiv
+	}
+	for _, val := range result {
+		resultMap[total_motiv] = append(resultMap[total_motiv], val)
 	}
 	return resultMap
 }
